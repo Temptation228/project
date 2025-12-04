1a0d2d4cdacafb7478c1acb9816a5a3e0f713c18
unknown <arstepanuk@mail.ru>
2025-12-04 23:37:25 +0300

    add json server, fix theme styles

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -25,6 +25,7 @@ export const Modal = (props: ModalProps) => {
 
     const [isClosing, setIsClosing] = useState(false);
     const timeRef = useRef<ReturnType<typeof setTimeout>>();
+    const { theme } = useTheme();
 
     const closeHandler = useCallback(() => {
         if (onClose) {
